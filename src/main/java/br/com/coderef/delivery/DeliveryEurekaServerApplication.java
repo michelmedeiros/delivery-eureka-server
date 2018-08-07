bf81f407c9fd0d6cf6c8bd44e231ea866c08d658
Michel Medeiros <michelf@ciandt.com>
2018-08-07 12:41:45 -0300

    First commit

---
+++ src/main/java/br/com/coderef/delivery/DeliveryEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.coderef.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryEurekaServerApplication.class, args);
+	}
+}
